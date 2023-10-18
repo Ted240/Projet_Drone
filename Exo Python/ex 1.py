703bb75ad1eee0b0b4e0f5d79e03a01601ef10d3
Teddy Barthelemy <barthelemyteddy24@gmail.com>
2023-10-18 10:31:37 +0200

    Exo 1-6 done

--- Exo Python/ex 1.py
+++ Exo Python/ex 1.py
@@ -1,3 +1,3 @@
 from math import pi
 r, h = tuple(map(float, [input(f"{x}: ") for x in ["Radius", "Height"]]))
-print(f"Volume = {(pi * r**2 * h) / 3.0}")
+print(f"Volume = {(pi * r**2 * h) / 3.0} m3")
---
+++ Exo Python/ex 5b.py
@@ -0,0 +1,1 @@
+print(f'Ce nombre est {"impair" if float(input("Number: ")) % 2 else "pair"}')
